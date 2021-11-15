ee8889e36d821e36524da62b6ed4f3ba73bf2d85
Nathan <nsrirama@smu.edu>
2021-11-15 17:09:03 -0600

    ok now the label's fixed

--- SwiftHTTPExample-InClassAssignment/HTTPSwiftExample/ModalViewController.swift
+++ SwiftHTTPExample-InClassAssignment/HTTPSwiftExample/ModalViewController.swift
@@ -28,6 +28,8 @@ class ModalViewController: UIViewController, UITextFieldDelegate,UIPickerViewDel
         self.ModelPicker.dataSource = self
         
         self.ModelPicker.selectRow(0, inComponent: 0, animated: true)
+        self.parameterLabel = "Penalty Number"
+        self.parameter2Label = "N/A"
         // Do any additional setup after loading the view.
     }
     
